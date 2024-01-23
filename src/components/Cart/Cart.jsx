e50a1c0ea6b589d1cfe8b963f6d35f25463bdcd5
Gopal Paul <gopal.kumar.paul1@gmail.com>
2024-01-23 14:17:39 +0600

    update to ordersummery of cart product

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,15 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShippingCarge = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShippingCarge = totalShippingCarge + product.shipping;
+        quantity = quantity + product.quantity
     }
 
     const tax = totalPrice*7/100;
@@ -18,7 +24,7 @@ const Cart = ({cart}) => {
   return (
     <div className="cart">
       <h5 className="cart-name">Order Summery</h5>
-      <p>Selected Items:{cart.length}</p>
+      <p>Selected Items:{quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Shipping Charge: ${totalShippingCarge}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
